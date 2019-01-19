67ae5018dad8e1c5887245edda2a5b1e34158597
Liudas <liudas.kasper@gmail.com>
2019-01-19 19:47:58 +0200

    Just the first test of remote branch

--- resources/views/repairs/create.blade.php
+++ resources/views/repairs/create.blade.php
@@ -7,7 +7,7 @@
 <div class="card big">
     <div class="card-header">
         <h2>
-            Pranešimo apie gedimą forma
+            Pranešimo apie gedimą forma.
         </h2>
     </div>
     <div class="card-body">
