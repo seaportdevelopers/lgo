d2cb33505734c30049221125263c59bee154a0e4
Liudas <liudas.kasper@gmail.com>
2019-03-09 14:45:37 +0200

    Added Manifacturers resource

---
+++ app/Http/Controllers/ManifController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Manif;
+use Illuminate\Http\Request;
+
+class ManifController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Manif  $manif
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Manif $manif)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Manif  $manif
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Manif $manif)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Manif  $manif
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Manif $manif)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Manif  $manif
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Manif $manif)
+    {
+        //
+    }
+}
