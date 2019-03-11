7dfd4d403e5d1172ce34cd5f876a2e5e697c9a18
Juozas Skarbalius <juozas@seaportdevelopers.lt>
2019-03-11 17:38:45 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -51019,8 +51019,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/idev/Documents/SeaportDevelopers/Logistics/lgo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/idev/Documents/SeaportDevelopers/Logistics/lgo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/sprucebird/Documents/Projects/lgo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/sprucebird/Documents/Projects/lgo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
